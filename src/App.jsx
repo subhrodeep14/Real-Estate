6ed414f1dcd5c2bf89b62614ad9a14cfac2c5905
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2024-11-15 01:01:38 +0530

    after about section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About"
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <div className="w-full overflow-hidden">
+    
+    <Header/>
+    <About/>
+    </div>
+  )
+}
+
+export default App
