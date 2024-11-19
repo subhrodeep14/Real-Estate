f22c2d221a88d391402a0c3919ce749509cc0c6a
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2024-11-19 13:01:15 +0530

    completed but animation not done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Project from "./components/Project"
 import Testimonials from "./components/Testimonials"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
 
   return (
     <div className="w-full overflow-hidden">
-    
+    <ToastContainer/>
     <Header/>
     <About/>
     <Project/>
     <Testimonials/>
+    <Contact/>
+    <Footer/>
     </div>
   )
 }
