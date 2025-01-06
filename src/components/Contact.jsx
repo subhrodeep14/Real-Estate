bb45b2be5db1f510bc306571d4e2f80d239efbf9
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-01-06 23:21:18 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,8 +38,6 @@ const Contact = () => {
     whileInView={{opacity:1,x:0}}
     viewport={{once:true}}
     
-    
-    
     className='text-center p-6 py-20 lg:px-32 w-full'>
       <h1 className='text-2xl font-bold sm:text-4xl mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move ? Let's Build Your Future Together</p>
